d735d4d1a44063ea485c7bcc68748e0bce91e395
shymoy <161142414+shymoy@users.noreply.github.com>
2024-09-10 06:22:35 +0800

    Update TestPalindrome.java

--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -2,8 +2,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class
-TestPalindrome {
+public class TestPalindrome {
     // You must use this palindrome, and not instantiate
     // new Palindromes, or the autograder might be upset.
     static Palindrome palindrome = new Palindrome();
