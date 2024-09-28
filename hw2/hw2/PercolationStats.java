029f1796d84ff731281da904ba592ba9ce9e51c4
shymoy <161142414+shymoy@users.noreply.github.com>
2024-09-28 19:30:42 +0800

    successful

---
+++ hw2/hw2/PercolationStats.java
@@ -0,0 +1,52 @@
+package hw2;
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+    private int times;
+    private final double[] thresholds;
+    PercolationFactory pf;
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        times = T;
+        pf = pf;
+        thresholds = new double[T];
+
+        for (int i = 0; i < times; i++) {
+            Percolation p = pf.make(N);
+            int openedSites = 0;
+
+            while (!p.percolates()) {
+                p.open(StdRandom.uniform(N), StdRandom.uniform(N));
+            }
+
+            openedSites = p.numberOfOpenSites();
+            thresholds[i] = (double) openedSites / (N * N);
+        }
+    }
+
+    public double mean() {
+        return StdStats.mean(thresholds);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(thresholds);
+    }
+
+    public double confidenceLow() {
+        double mean = mean();
+        double stddev = stddev();
+        return mean - 1.96 * stddev / Math.sqrt(times);
+    }                                  // low endpoint of 95% confidence interval
+
+    public double confidenceHigh() {
+        double mean = mean();
+        double stddev = stddev();
+        return mean + 1.96 * stddev / Math.sqrt(times);
+    }                              // high endpoint of 95% confidence interval
+}
